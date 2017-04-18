2f287943c74d6aefbc3da29dd65e36115e8785c7
Hisahiro Tsukamoto <hishats@gmail.com>
2017-04-18 15:56:27 +0900

    Make symlinks to dotfiles

--- makesymlink.sh
+++ makesymlink.sh
@@ -1,4 +1,6 @@
 # /bin/bash
 ln -sf ~/github/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/github/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/github/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/github/dotfiles/.ssh/config ~/.ssh/config
 
