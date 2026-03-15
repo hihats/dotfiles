89903e697a69af6f03b7eebf0332acc9a0f793ff
Hisahiro Tsukamoto <hishats@gmail.com>
2026-03-15 23:35:23 +0900

    mapleader の設定を init.lua の先頭（require("config.lazy") の前）に移動

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- Leader key (must be set before lazy.nvim)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Bootstrap lazy.nvim & plugins
 require("config.lazy")
 
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,5 @@
 local map = vim.keymap.set
 
--- Leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 -- Better window navigation
 map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
